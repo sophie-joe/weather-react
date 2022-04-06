4ae191d569d412b7472b7145f5bd732f76ce2e7f
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-04-06 17:35:04 +1200

    Update failure for best practice, no referrer

--- src/Built-By.js
+++ src/Built-By.js
@@ -4,11 +4,19 @@ import "./Forecast.css";
 export default function Built() {
   return (
     <p className="Forecast">
-      <a href="https://github.com/sophie-joe/weather-react" target="_blank">
+      <a
+        href="https://github.com/sophie-joe/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>{" "}
       by{" "}
-      <a href="https://www.shecodes.io/students/545-sophie-joe" target="_blank">
+      <a
+        href="https://www.shecodes.io/students/545-sophie-joe"
+        target="_blank"
+        rel="noreferrer"
+      >
         Sophie Joe
       </a>
     </p>
