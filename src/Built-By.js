facb48291f776cc8965579c2b665d97f6e6b6cd7
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-04-06 17:29:31 +1200

    Open links in tabs

--- src/Built-By.js
+++ src/Built-By.js
@@ -4,9 +4,13 @@ import "./Forecast.css";
 export default function Built() {
   return (
     <p className="Forecast">
-      <a href="https://github.com/sophie-joe/weather-react">Open-source code</a>{" "}
+      <a href="https://github.com/sophie-joe/weather-react" target="_blank">
+        Open-source code
+      </a>{" "}
       by{" "}
-      <a href="https://www.shecodes.io/students/545-sophie-joe">Sophie Joe</a>
+      <a href="https://www.shecodes.io/students/545-sophie-joe" target="_blank">
+        Sophie Joe
+      </a>
     </p>
   );
 }
